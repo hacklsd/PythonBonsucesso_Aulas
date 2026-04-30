f38b5e4c37b7dcea0cf98ad72365e638291f7574
Hack_Mind_LSD <vicbarbeiro@gmail.com>
2026-04-29 21:41:08 -0300

    Add files via upload

---
+++ Aula2904/aula2904.py
@@ -0,0 +1,33 @@
+#   • SINATAXE: FORMA CORRETA DE ORDENAR E ESCREVER INSTRUÇÕES NO CÓDIGO
+ 
+#   nome_da_variavel = valor
+#   EX: 
+#   nome = "Victor"     (string = letras, identifica-se pelas aspas, na atribuição do valor.)
+#   idade = 33          (Integer = número inteiro, identificado pelo número sem vírgula.
+#   altura = 1,75       (float = número decimal, identificado por casa decimal, uso de vírgula.)
+#   estudando = True    (Boolean = booleano, expressa unicamente verdadeiro ou falso (True - False).)
+
+#   • OPERADORES ARITMÉTICOS: OPERAR MATEMATICAMENTE NO CÓDIGO
+
+#   + ADIÇÃO                → x + y
+#   - SUBTRAÇÃO             → x - y
+#   * MULTIPLICAÇÃO         → x * y
+#   / DIVISÃO               → x / y
+#   % MÓDULO OU RESTO       → x % y
+#   ** EXPONENCIAÇÃO        → x %% y
+#   // ARREDONDAR DIVISÃO   → x // y
+
+#   • CONCATENAÇÃO: JUNÇÃO DE DOIS VALORES JÁ ATRIBUÍDOS
+
+#   a = "Hello "
+#   b = "World"
+#   c = a + b 
+#   print(c)    
+#   → RESULTADO: Hello World
+
+#   • INPUT: ENTRADA DE DADOS ATRAVÉS DO USUÁRIO
+
+#   print("Qual seu nome?")
+#   x = input()    → EXEMPLO: SUPONHA QUE O USUÁRIO DIGITOU "VICTOR"
+#   print = ("Hello, " + x)
+#   → RESULTADO: Hello, Victor
