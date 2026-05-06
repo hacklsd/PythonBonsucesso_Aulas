dda5e485619b5ae45ade8a2045b96af307ec09cb
Hack_Mind_LSD <vicbarbeiro@gmail.com>
2026-05-06 19:03:15 -0300

    Add files via upload

---
+++ Aula0605/Aula0605.py
@@ -0,0 +1,6 @@
+"""
+1.  Revisando: concatenação de int e str..
+2.  Revisando: IF, ELSE, ELIF...
+
+"""
+
---
+++ Aula0605/script0605.py
@@ -0,0 +1,7 @@
+nome = str(input('Insira seu nome: '))
+idade = int(input('Insira sua idade: '))
+ano = 2026 - idade
+
+
+print(nome, ", você nasceu em ", ano)
+
